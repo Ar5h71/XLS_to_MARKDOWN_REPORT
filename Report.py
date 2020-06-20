048b050c20f188c041d65ec2350845a973cd3460
Ar5h71 <50661938+Ar5h71@users.noreply.github.com>
2020-06-20 19:24:03 +0530

    Update Report.py

--- Report.py
+++ Report.py
@@ -1,82 +1,128 @@
+# This program generates according to the template provided in 'template.txt' file in the template folder
+
+# The report is written in Markdown format in 'report.md' file in markdown folder
+
+
 import pandas as pd
 import os
 import glob
 
 
-def column_location(combined_sheet, indices):
-    # determines the location of column headers according to the template file
-    col_loc = []
-    for i in range(len(indices)):
-        for j in range(combined_sheet.shape[1]):
-            if indices[i] in combined_sheet.columns[j]:
-                col_loc.append(j)
-    return col_loc
+def column_location(spreadsheet, column_names):
+    """
+    This function finds the indices of columns in the order specified in template file.
+
+        Parameters:
+            spreadsheet (DataFrame): A pandas DataFrame.
+            column_names (List of srtings): list of strings that specifies the name of column headers as in template file.
+
+        Returns:
+            column_location (list of integers): list of integers specifying the column locations in the DataFrame according tp the template specified.
+    """
+    column_location = []
+    for i in range(len(column_names)):
+        for j in range(spreadsheet.shape[1]):
+            if column_names[i] in spreadsheet.columns[j]:
+                column_location.append(j)
+    return column_location
 
 
-def date_sort_and_format(combined_sheet, length ,separator):
+def date_sort_and_format(list_sheets, separator):
+    """
+    Returns the DataFrame after sorting the data in descending order of date if there is any column that contains date
+    in the DataFrame.
+
+        Parameters:
+            list_sheets (list of dataframes): a list of Pandas DataFrames.
+            separator (character): a special character to separate the day month and year
+
+        Returns:
+            all_sheets (list of DataFrames): a list of DataFrames with all data sorted in descending order of date column(if any) and format of date converted to ddmmyyyy
+    """
     all_sheets = []
-    if length == 1:
-        all_sheets.append(combined_sheet)
-    else:
-        for i in range(length):
-            all_sheets.append(combined_sheet[i])
+    for i in range(len(list_sheets)):
+        all_sheets.append(list_sheets[i])
 
-    for j in range(length):
+    for j in range(len(all_sheets)):
         for i in range(all_sheets[j].shape[1]):
             if all_sheets[j].columns[i][0] != '_' and 'Date' in all_sheets[j].columns[i]:
                 all_sheets[j][all_sheets[j].columns[i]] = pd.to_datetime(all_sheets[j][all_sheets[j].columns[i]])
                 all_sheets[j] = all_sheets[j].sort_values(by=[all_sheets[j].columns[i]], ascending=False)
                 break
 
-    for j in range(length):
+    for j in range(len(all_sheets)):
         for i in range(all_sheets[j].shape[1]):
             if all_sheets[j].columns[i][0] != '_' and 'Date' in all_sheets[j].columns[i]:
                 all_sheets[j][all_sheets[j].columns[i]] = pd.to_datetime(all_sheets[j][all_sheets[j].columns[i]])
-                all_sheets[j][all_sheets[j].columns[i]] = all_sheets[j][all_sheets[j].columns[i]].dt.strftime("%d" + separator + "%m" + separator + "%Y")
-
-    if length == 1:
-        return all_sheets[0]
-    else:
-        return all_sheets
+                all_sheets[j][all_sheets[j].columns[i]] = all_sheets[j][all_sheets[j].columns[i]].dt.strftime(
+                    "%d" + separator + "%m" + separator + "%Y")
+    return all_sheets
 
 
-def gen_combined_sheet(sheets):
-    # merges the sheets on the basis of '_uuid' column that contains unique id for submissions
+def gen_list_sheets(sheets):
+    """
+    Returns a list of DataFrames which can be worked upon to generate reports.
+
+    This function takes a list of dataframes which are all the sheets in an excel file and merges the relevant sheets
+    according to the unique id column '_uuid'
+
+        Parameters:
+            sheets (list of DataFrames): A list of pandas DataFrames. All the sheets of the excel file opened as DataFrames and stored in a list
+
+        Returns:
+            all_sheets (List of DataFrames):A list of DataFrames after performing the merging operation.
+    """
     all_sheets = []
-    if len(sheets) == 1:
-        return sheets[0]        # returns the first sheet if there is only one
+    if len(sheets) == 1:       # if there is only one sheet in the file then it returns the sheet itself
+        all_sheets.append(sheets[0])
+        return all_sheets  # returns the first sheet if there is only one
     all_sheets.append(pd.merge(sheets[0], sheets[1], on='_uuid'))
-    if (len(sheets)) == 2:
-        return all_sheets[0]    # if there are only 2 sheets then merges both and returns the combined sheet
+    if (len(sheets)) == 2:     # if the form has 2 sheets then both are merged according to '_uuid' column and #
+        return all_sheets      # returned as a dataframe
     for j in range(len(sheets) - 2):
         all_sheets.append(pd.merge(all_sheets[0], sheets[j + 2], on='_uuid'))
-    return all_sheets[1:]       # if more than 2 sheets i.e. multi-category form then combines 3rd sheet onwards
-                                # to 1st and 2nd sheet and returns a list of combined sheets
+                                    # if more than 2 sheets i.e. multi-category form then combines 3rd sheet onwards
+    return all_sheets[1:]           # to 1st and 2nd sheet and returns a list of combined sheets
 
 
-def write_table(combined_sheet, indices, f):
-    for i in range(len(indices)):
-        f.write('| ' + indices[i])
+def write_table(combined_sheet, column_names, f):
+    """
+    Writes the data of DataFrame in tabular format in Markdown.
+
+        Parameters:
+            combined_sheet (DataFrame): A Pandas DataFrame whose data is to be written in Tabular Form in Markdown.
+            column_names (List of Strings): A list of strings containing all the relevant column names of the DataFrame.
+            f (file object): file object to write the data in 'report.md'
+    """
+    for i in range(len(column_names)):
+        f.write('| ' + column_names[i])
     f.write(' |\n')
 
-    for i in range(len(indices)):
+    for i in range(len(column_names)):
         f.write('|-----')
     f.write(' |\n')
-    col_loc = column_location(combined_sheet, indices)
+    col_loc = column_location(combined_sheet, column_names)
 
     for i in range(combined_sheet.shape[0]):
         for j in range(len(col_loc)):
             for k in range(len(combined_sheet.iloc[i, col_loc[j]])):
                 if combined_sheet.iloc[i, col_loc[j]][k] == '\n':
-                    combined_sheet.iloc[i, col_loc[j]] = combined_sheet.iloc[i, col_loc[j]][:k - 1] + ' <br> ' + \
-                                                         combined_sheet.iloc[i, col_loc[j]][k + 1:]
+                    combined_sheet.iloc[i, col_loc[j]] = combined_sheet.iloc[i, col_loc[j]][:k - 1] + ' <br> ' + combined_sheet.iloc[i, col_loc[j]][k + 1:]
             f.write('| ' + combined_sheet.iloc[i, col_loc[j]])
         f.write(' |\n')
     f.write('<br><br><br>\n')
 
 
-def write_list(combined_sheet, indices, f):
-    col_loc = column_location(combined_sheet, indices)
+def write_list(combined_sheet, column_names, f):
+    """
+    Writes the data of DataFrame in list format in Markdown.
+
+        Parameters:
+            combined_sheet (DataFrame): A Pandas DataFrame whose data is to be written in list Form in Markdown.
+            column_names (List of strings): A list of strings containing all the relevant column names of the DataFrame.
+            f (file object): file object to write the data in 'report.md'
+    """
+    col_loc = column_location(combined_sheet, column_names)
 
     for i in range(combined_sheet.shape[0]):
         f.write('- ')
@@ -92,76 +138,90 @@ def write_list(combined_sheet, indices, f):
 
 
 def rename_submission__uuid(sheets):
-    # the unique id column name is '_uuid' in the first sheet and '_submission__uuid' in the subsequent sheets
-    # this function renames '_submission__uuid' to '_uuid' so that the sheets can be merged
+    """
+    Renames the '_submission__uuid' column to '_uuid'.
+
+    The unique id column name is '_uuid' in the first sheet and '_submission__uuid' in the subsequent sheets.
+    This function renames '_submission__uuid' to '_uuid' so that the sheets can be merged.
+
+        Parameters:
+            sheets (list of DataFrames): a list of DataFrames containing data of all the sheets in an excel file.
+
+        Returns:
+            sheets (list of DataFrames): list of DataFrames with '_submission__uuid' renamed as '_uuid'
+    """
     for j in range(len(sheets)):
         sheets[j].rename(columns={'_submission__uuid': '_uuid'}, inplace=True)
     return sheets
 
 
 def xls2md(excel_spreadsheet, file_name, template, f, separator):
+    """
+    Writes all data to 'reprt.md' file in markdown format
+
+    This function takes in the excel file, its name, its specified template, file object and separator to write in dates
+    and wrtes that data in tabular or list format as specified in the template.
+
+        Parameters:
+            excel_spreadsheet (DataFrame): DataFrame of all the excel spreadsheeets in a file.
+            file_name (string): String specifying name of the excel file.
+            template (list of strings): A list of string specifying the template for the Excel file
+            f (file object): File object to write data in Markdown format in the file 'report.md'
+            separator (character): A character for separating the day, month and year for the dates in the report.
+
+    """
+
+    sheetnames = excel_spreadsheet.sheet_names
+    sheets = []
+
+    for m in range(len(sheetnames)):  # stores all sheets in an excel file in a list 'sheet'
+        sheets.append(pd.read_excel(excel_spreadsheet, sheetnames[m]))
+    column_names = template[3:len(template) - 1]  # gets a list of names of column headers in a sheet
+
+    sheets = rename_submission__uuid(sheets)
+    list_sheets = gen_list_sheets(sheets)
+    list_sheets = date_sort_and_format(list_sheets, separator)
+
+    for k in range(len(list_sheets)):       # this loop converts all data of DataFrame to string format
+        for i in range(list_sheets[k].shape[0]):
+            for j in range(list_sheets[k].shape[1]):
+                list_sheets[k].iloc[i, j] = str(list_sheets[k].iloc[i, j])
+
     f.write('## **')
 
-    for i in range(file_name.index('_-_')):       # determines the form name and converts it to all caps
-        if file_name[i] !='_':
+    for i in range(file_name.index('_-_')):  # determines the form name from file name and converts it to all caps
+        if file_name[i] != '_':  # and writes it in 'report.md'
             f.write(file_name[i].upper())
         else:
             f.write(' ')
 
-    spreadsheet_sheets = excel_spreadsheet.sheet_names
-    sheet = []
-
-    for m in range(len(spreadsheet_sheets)):      # stores all sheets in an excel file in a list 'sheet'
-        sheet.append(pd.read_excel(excel_spreadsheet, spreadsheet_sheets[m]))
-    indices = template[3:len(template) - 1]               # gets a list of names of column headers in a sheet
-
-    sheet = rename_submission__uuid(sheet)
-    combined_sheet = gen_combined_sheet(sheet)
-
-    if len(sheet)<=2:               # merges the sheets in the excel file according to the column '_uuid'
+    if len(sheets)<=2:         # if the form has single set of repeating questions or no repeating questions
         f.write('**\n')
-
-        combined_sheet = date_sort_and_format(combined_sheet, 1, separator)
-
-        for i in range(combined_sheet.shape[0]):
-            for j in range(combined_sheet.shape[1]):
-                combined_sheet.iloc[i,j] = str(combined_sheet.iloc[i,j])
-
         if template[0] == 't':
-            write_table(combined_sheet, indices, f)
+            write_table(list_sheets[0], column_names, f)
         elif template[0] == 'l':
-            write_list(combined_sheet, indices, f)
-
-    else:
+            write_list(list_sheets[0], column_names, f)
+    else:             # if the form has multiple sets of repeating questions the=at vary according to category selected.
         f.write(' - ' + template[2].replace('_', ' ').upper() + '**\n')
-
-        combined_sheet = date_sort_and_format(combined_sheet, len(combined_sheet), separator)
-
-        for i in range(len(combined_sheet)):
-            for j in range(combined_sheet[i].shape[0]):
-                for k in range(combined_sheet[i].shape[1]):
-                    combined_sheet[i].iloc[j, k] = str(combined_sheet[i].iloc[j, k])
-
-        for l in range(2, len(sheet)):
-            if template[2] == spreadsheet_sheets[l]:
+        for l in range(2, len(sheets)):     # checks for the correct sheetname as in the template
+            if template[2] == sheetnames[l]:
                 break
-
-        if template[0] == 't':
-            write_table(combined_sheet[l-2], indices, f)
+        if template[0] == 't':          # l-2 because the categories of form start from sheet number 3.
+            write_table(list_sheets[l - 2], column_names, f)
         elif template[0] == 'l':
-            write_list(combined_sheet[l-2], indices, f)
+            write_list(list_sheets[l - 2], column_names, f)
 
 
-cur_path = os.getcwd()  # gets the path where the python file is located
+cur_path = os.getcwd()
 template_path = cur_path + r'\template'
 path = cur_path + r'\excel'
 template = open(template_path + '\\' + 'template.txt')
-template_fields = template.readlines()
+template_fields = template.readlines()      # gets the template for all the forms from 'template.txt' file line by line
 
 for i in range(len(template_fields)):
     template_fields[i] = template_fields[i].split(';')
 
-files = [f for f in glob.glob(path + '**/*.xlsx', recursive=True)] # gets the paths for all the excel files stored in 'excel' folder
+files = [f for f in glob.glob(path + '**/*.xlsx',recursive=True)]  # gets the paths for all the excel files stored in 'excel' folder
 filename = []
 lenpath = len(path) + 1
 dest_path = os.getcwd() + '\\'
@@ -171,13 +231,13 @@ sep = str(input("which separator do you want for dates? Please Enter\n"))
 f = open(dest_path, 'w')
 
 for file in files:
-    filename.append(file[lenpath:len(file) - 5])          #gets the filenames of all the files in the 'excel' folder
+    filename.append(file[lenpath:len(file) - 5])  # gets the filenames of all the files in the 'excel' folder
 
 for k in range(len(template_fields)):
     for i in range(len(filename)):
         if filename[i] in template_fields[k][1]:
             break
-    spreadsheet = pd.ExcelFile(files[i])   #opens all the excel files one by one
-    xls2md(spreadsheet, filename[i], template_fields[k],f, sep)     #the function that converts the excel spreadsheet to md
+    spreadsheet = pd.ExcelFile(files[i])  # opens all the excel files one by one
+    xls2md(spreadsheet, filename[i], template_fields[k], f, sep)  # the function that converts the excel spreadsheet to md
 
 print("All Files have been converted to Markdown")
